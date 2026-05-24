2a16d1b66875d9bc21f83cb45dd96101336ffce2
Vinicius <macuco.vinicius@estudante.ufjf.br>
2026-05-24 16:59:57 -0300

    Revert "adicionndando wrapper de testes"
    This reverts commit ef9b8eaa6e7e78a0fc1786695fc46a40f9f3e8d3.

--- headders/TestAll.h
+++
@@ -1,9 +0,0 @@
-#ifndef TESTALL_H
-#define TESTALL_H
-
-class TestAll {
-public:
-    static void testAll();
-};
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-#include "headders/TestAll.h"
+#include <iostream>
+#include "headders/Graph.h"
+#include "headders/GraphReader.h"
 
 
 int main() {
-    TestAll::testAll();
+    
+
+    Graph graph(Representation::MATRIX, true, true);
+
+    graph.runTests();
+
+
+    std::string caminhoDoArquivo = "GraphFile_Model.graph"; 
+    
+    Graph* meuGrafoCarregado = GraphReader::readGraphFile(caminhoDoArquivo);
+
+    
+    meuGrafoCarregado->print();
+
+    delete meuGrafoCarregado; // verificar a possibilidade de passar o grafo por valor para evirar memoryleak
+
+
+    // ListGraph Lgraph= ListGraph();
+
+    // Lgraph.runIntensiveTests();
+
+    // MatrixGraph Mgraph;
+    
+    // Mgraph.runIntensiveTests();
+
     return 0;
 }
